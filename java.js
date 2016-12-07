853202257fe8fe61b356dd734cfda66441adf4c8
Eric R. Howard <ehoward@hudsonhs.com>
2016-12-07 22:56:02 +0000

    started to connect spacebar to jquery event

---
+++ java.js
@@ -0,0 +1,15 @@
+// JavaScript File
+$(document).ready(function() {
+
+    $("body").keydown(function(event) {
+        if (event.keyCode == 32) {
+
+
+            //   event.preventDefault();
+            alert("Handler for .keydown() called.");
+
+        }
+
+    });
+
+});
